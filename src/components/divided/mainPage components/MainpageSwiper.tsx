7e3d4e8bd09c11ed6949984c0fbcf4d0698c0e29
Riiru <burhanov774@gmail.com>
2024-04-20 17:58:54 +0600

    верстал mainpage

---
+++ src/components/divided/mainPage components/MainpageSwiper.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import MenuButton from "../MenuButton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "../../../features/swiper/swiper.css";
+import phoneImg from "../../../img/mainMenu/phoneImg.jpg";
+import catalogueImg from "../../../img/mainMenu/catalogueImg.png";
+import megaphoneImg from "../../../img/mainMenu/megaphoneImg.png";
+import assemblePC from "../../../img/mainMenu/assemblePC.png";
+import fireEmoji from "../../../img/mainMenu/fireEmoji.png";
+import shieldImg from "../../../img/mainMenu/shieldImg.png";
+import gear from "../../../img/mainMenu/gear.png";
+import question from "../../../img/mainMenu/question.png";
+import classes from "../../../pages/mainpage/Mainpage.module.css";
+
+const MainpageSwiper = () => {
+  return (
+    <div>
+      <div className={classes.mainpage__row}>
+        <MenuButton
+          width={283}
+          height={206}
+          title="Личный кабинет"
+          text="Получайте бонусы, отслеживайте заказы и делитесь мнением"
+          background="#fff7da"
+          innerButton={true}
+          marginRight="10px"
+          imageSrc={phoneImg}
+        />
+        <div className={classes.rightside__menu}>
+          <Swiper
+            slidesPerView={6}
+            spaceBetween={30}
+            freeMode={true}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[FreeMode]}
+          >
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Каталог"
+                text="Большой выбор комплектующих"
+                background="rgb(229, 244, 251)"
+                imageSrc={catalogueImg}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Акции"
+                text="Скидки, рассрочки, выгодные комплекты"
+                background="rgb(235, 255, 237)"
+                imageSrc={megaphoneImg}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Собрать ПК"
+                text="Без проблем с совместимостью"
+                background="rgb(242, 243, 245)"
+                imageSrc={assemblePC}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Готовые сборки"
+                text="Горячие сборки ПК от пользователей"
+                background="rgb(255, 239, 239)"
+                imageSrc={fireEmoji}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Сервис"
+                text="Гарантия, возврат, обмен и ремонт"
+                background="rgb(236, 249, 255)"
+                imageSrc={shieldImg}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Тесты"
+                text="Проверка и идентификация комплектующих вашего ПК"
+                background="rgb(252, 246, 215)"
+                imageSrc={gear}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <MenuButton
+                width={183}
+                height={206}
+                title="Помощь"
+                text="Частые вопросы, полезная информация"
+                background="rgb(255, 239, 239)"
+                imageSrc={question}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainpageSwiper;
--- src/pages/mainpage/Mainpage.tsx
+++ src/pages/mainpage/Mainpage.tsx
@@ -1,113 +1,12 @@
 import classes from "./Mainpage.module.css";
 import ReduxTester from "../../components/divided/ReduxTester";
-import MenuButton from "../../components/divided/MenuButton";
-import "../../features/swiper/swiper.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "../../features/swiper/swiper.css";
-import phoneImg from "../../img/mainMenu/phoneImg.jpg";
-import catalogueImg from "../../img/mainMenu/catalogueImg.png";
-import megaphoneImg from "../../img/mainMenu/megaphoneImg.png";
-import assemblePC from "../../img/mainMenu/assemblePC.png";
-import fireEmoji from "../../img/mainMenu/fireEmoji.png";
+import MainpageSwiper from "../../components/divided/mainPage components/MainpageSwiper";
 
 const Mainpage = () => {
   return (
     <div className={classes.wrapper}>
-      <div className={classes.mainpage__row}>
-        <MenuButton
-          width={283}
-          height={206}
-          title="Личный кабинет"
-          text="Получайте бонусы, отслеживайте заказы и делитесь мнением"
-          background="#fff7da"
-          innerButton={true}
-          marginRight="10px"
-          imageSrc={phoneImg}
-        />
-        <div className={classes.rightside__menu}>
-          <Swiper
-            slidesPerView={6}
-            spaceBetween={30}
-            freeMode={true}
-            loop={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[FreeMode]}
-          >
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Каталог"
-                text="Большой выбор комплектующих"
-                background="rgb(229, 244, 251)"
-                imageSrc={catalogueImg}
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Акции"
-                text="Скидки, рассрочки, выгодные комплекты"
-                background="rgb(235, 255, 237)"
-                imageSrc={megaphoneImg}
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Собрать ПК"
-                text="Без проблем с совместимостью"
-                background="rgb(242, 243, 245)"
-                imageSrc={assemblePC}
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Готовые сборки"
-                text="Горячие сборки ПК от пользователей"
-                background="rgb(255, 239, 239)"
-                imageSrc={fireEmoji}
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Сервис"
-                text="Гарантия, возврат, обмен и ремонт"
-                background="rgb(236, 249, 255)"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Тесты"
-                text="Проверка и идентификация комплектующих вашего ПК"
-                background="rgb(252, 246, 215)"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <MenuButton
-                width={183}
-                height={206}
-                title="Помощь"
-                text="Частые вопросы, полезная информация"
-                background="rgb(255, 239, 239)"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
+      <MainpageSwiper />
+      <div className={classes.actual_offers_row}></div>
     </div>
   );
 };
