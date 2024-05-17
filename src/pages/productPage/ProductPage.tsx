77d40014fbc5d1aa972d4e9955dfb3e7bc551bb6
Riiru <burhanov774@gmail.com>
2024-05-17 07:59:48 +0600

    верстал страницу с товаром, а точнее main menu карточку товара

--- src/pages/productPage/ProductPage.tsx
+++ src/pages/productPage/ProductPage.tsx
@@ -2,9 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "../../helper/supabaseClient";
 import Header from "../../components/global/header/Header";
+import classes from "./ProductPage.module.css";
+import starImg from "../../img/searchPage/star.svg";
 
 interface Product {
   id: string;
+  cpuName: string;
+  img: string;
+  socket: string;
+  frequency: number;
+  cacheL2: number;
+  cacheL3: number;
+  coresNumber: number;
+  DDR: string;
+  ramFrequency: number;
+  TDP: number;
 }
 
 const ProductPage: React.FC = () => {
@@ -17,10 +29,11 @@ const ProductPage: React.FC = () => {
     try {
       const { data } = await supabase
         .from("cpu")
-        .select("id")
+        .select("*")
         .eq("id", `${parsedProductId}`);
-      if (data && data.length > 0) {
+      if (data) {
         setProduct(data[0]);
+        console.log(data);
       }
     } catch (error) {
       console.error("Error fetching supabase data", error);
@@ -39,8 +52,35 @@ const ProductPage: React.FC = () => {
   return (
     <div>
       <Header />
-      <h2>qwrefasd</h2>
-      <h3>{product.id + 1}</h3>
+      <div className={classes.product_background}>
+        <div className={classes.product_container}>
+          <h1>Процессор {product.cpuName}</h1>
+          <div className={classes.product_card}>
+            <div className={classes.product_card_main}>
+              <img
+                src={product.img}
+                alt="product img"
+                className={classes.product_img}
+              />
+              <div className={classes.product_characteristics}>
+                <h3>
+                  {product.socket}, {product.coresNumber} x {product.frequency}{" "}
+                  ГГц, L2 - {product.cacheL2} МБ, L3 - {product.cacheL3} МБ, 2 x{" "}
+                  {product.DDR}-{product.ramFrequency} МГц, TDP {product.TDP} Вт
+                </h3>
+                <div className={classes.stars}>
+                  <img src={starImg} alt="rating img" />
+                  <img src={starImg} alt="rating img" />
+                  <img src={starImg} alt="rating img" />
+                  <img src={starImg} alt="rating img" />
+                  <img src={starImg} alt="rating img" />
+                  <h4>10</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
