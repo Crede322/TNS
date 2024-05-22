cff9e812fd6e3f8f775df0608f15a67f0825f6f8
Riiru <burhanov774@gmail.com>
2024-05-22 08:41:11 +0600

    Updates

---
+++ static/js/952.0e1be8cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktns=self.webpackChunktns||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0e1be8cb.chunk.js.map
