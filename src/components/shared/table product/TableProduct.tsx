44407881a79fc10bc867f461933b9713a0b84a24
Riiru <burhanov774@gmail.com>
2024-06-14 07:51:15 +0600

    пофиксил favButton

--- src/components/shared/table product/TableProduct.tsx
+++ src/components/shared/table product/TableProduct.tsx
@@ -1,8 +1,7 @@
 import classes from "./TableProduct.module.css";
-import { Link } from "react-router-dom";
 import starImg from "../../../img/searchPage/star.svg";
-import imgFavorite from "../../../img/favorite.svg";
-
+import FavButton from "../Fav button/FavButton";
+import { useNavigate } from "react-router-dom";
 interface TableProductProps {
   product: Product;
 }
@@ -19,64 +18,57 @@ interface Product {
   img: string;
 }
 
-const handlePurchaseClick = (productId: number) => {
-  console.log("Navigating to product:", productId);
-};
-
-const handleFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
-  event.preventDefault();
-  console.log("favorite");
-};
-
 const TableProduct: React.FC<TableProductProps> = ({ product }) => {
+  const navigate = useNavigate();
+  const handlePurchaseClick = (productId: number) => {
+    console.log("Navigating to product:", productId);
+    navigate(`/product/${productId}`);
+  };
+
   return (
-    <Link key={product.id} to={`/product/${product.id}`}>
-      <div
-        className={classes.result}
-        onClick={() => handlePurchaseClick(product.id)}
-      >
-        <div className={classes.result__product_image}>
-          <img src={product.img} alt="product img" />
+    <div
+      className={classes.result}
+      onClick={() => handlePurchaseClick(product.id)}
+    >
+      <div className={classes.result__product_image}>
+        <img src={product.img} alt="product img" />
+      </div>
+
+      <div className={classes.result_description}>
+        <h3 className={classes.product_info}>
+          {product.cpuName} <br />[{product.socket}, {product.coresNumber} x{" "}
+          {product.frequency} ГГц, L2 - {product.cacheL2} МБ, L3 -{" "}
+          {product.cacheL3} МБ]
+        </h3>
+        <div className={classes.purchase}>
+          <h2>{product.price}</h2>
+
+          <button
+            className={classes.purchase_button}
+            onClick={() => handlePurchaseClick(product.id)}
+          >
+            <h3>Купить</h3>
+          </button>
+
+          <FavButton favStyle="mainPageStyle" id={product.id} />
         </div>
 
-        <div className={classes.result_description}>
-          <h3 className={classes.product_info}>
-            {product.cpuName} <br />[{product.socket}, {product.coresNumber} x{" "}
-            {product.frequency} ГГц, L2 - {product.cacheL2} МБ, L3 -{" "}
-            {product.cacheL3} МБ]
-          </h3>
-          <div className={classes.purchase}>
-            <h2>{product.price}</h2>
-
-            <button
-              className={classes.purchase_button}
-              onClick={() => handlePurchaseClick(product.id)}
-            >
-              <h3>Купить</h3>
-            </button>
-
-            <button className={classes.fav_button} onClick={handleFavorite}>
-              <img src={imgFavorite} alt="imgFavorite" />
-            </button>
+        <div className={classes.product_subInfo}>
+          <div className={classes.stars}>
+            <img src={starImg} alt="rating img" />
+            <img src={starImg} alt="rating img" />
+            <img src={starImg} alt="rating img" />
+            <img src={starImg} alt="rating img" />
+            <img src={starImg} alt="rating img" />
+            <h4>10</h4>
           </div>
-
-          <div className={classes.product_subInfo}>
-            <div className={classes.stars}>
-              <img src={starImg} alt="rating img" />
-              <img src={starImg} alt="rating img" />
-              <img src={starImg} alt="rating img" />
-              <img src={starImg} alt="rating img" />
-              <img src={starImg} alt="rating img" />
-              <h4>10</h4>
-            </div>
-            <div>
-              <h4>В наличии</h4>
-              <h3>Послезавтра</h3>
-            </div>
+          <div>
+            <h4>В наличии</h4>
+            <h3>Послезавтра</h3>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
