c7f6fe001575b1c6610d73a8cefa83d30c27fa08
Crede322 <burhanov774@gmail.com>
2025-05-28 16:48:50 +0500

    feat(mainpage, productpage): адаптировал несколько сегментов под разные устройства: actualOffers from mainpage, productpage

--- src/pages/mainPage/mainPage components/actual offers/ActualOffers.tsx
+++ src/pages/mainPage/mainPage components/actual offers/ActualOffers.tsx
@@ -1,140 +1,84 @@
 import classes from "./ActualOffer.module.css";
+import { Link } from "react-router-dom";
+import OfferExemplar from "./OfferExemplar";
 
 const ActualOffer: React.FC = () => {
   return (
     <div className={classes.actual_offers_row}>
-      <div className={classes.actual_offer_wrapper}>
-        <a
-          href="https://crede322.github.io/TNS/#/search/?q=ryzen%205"
-          className={classes.actual_offer_title}
-        >
-          Ryzen 5
-        </a>
-        <div className={classes.content}>
-          <a
-            href="https://crede322.github.io/TNS/#/search/?q=ryzen%205"
-            className={classes.offer_button}
-          >
-            <img
-              src="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/ryzen5"
-              alt="actual offer"
-            />
-          </a>
-          <div className={classes.offer_categories}>
-            <a href="https://crede322.github.io/TNS/#/product/6">
-              <h5>AMD Ryzen 5 1600</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/9">
-              <h5>AMD Ryzen 5 3600X</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/15">
-              <h5>AMD Ryzen 5 5600X</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/2">
-              <h5>AMD Ryzen 5 3600</h5>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className={classes.actual_offer_wrapper}>
-        <a
-          href="https://crede322.github.io/TNS/#/search/?q=ryzen%207"
-          className={classes.actual_offer_title}
-        >
-          Ryzen 7
-        </a>
-        <div className={classes.content}>
-          <a
-            href="https://crede322.github.io/TNS/#/search/?q=ryzen%207"
-            className={classes.offer_button}
-          >
-            <img
-              src="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/ryzen7"
-              alt="actual offer"
-            />
-          </a>
-          <div className={classes.offer_categories}>
-            <a href="https://crede322.github.io/TNS/#/product/7">
-              <h5>AMD Ryzen 7 1700X</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/5">
-              <h5>AMD Ryzen 7 2700X</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/1">
-              <h5>AMD Ryzen 7 3700x</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/11">
-              <h5>AMD Ryzen 7 7800X3D</h5>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className={classes.actual_offer_wrapper}>
-        <a
-          href="https://crede322.github.io/TNS/#/search/?q=core%20i5"
-          className={classes.actual_offer_title}
-        >
-          Core i5
-        </a>
-        <div className={classes.content}>
-          <a
-            href="https://crede322.github.io/TNS/#/search/?q=core%20i5"
-            className={classes.offer_button}
-          >
-            <img
-              src="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/i5%2012600k?t=2024-08-18T06%3A20%3A22.262Z"
-              alt="actual offer"
-            />
-          </a>
-          <div className={classes.offer_categories}>
-            <a href="https://crede322.github.io/TNS/#/product/110">
-              <h5>Intel Core i5-12400</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/104">
-              <h5>Intel Core i5-13400F</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/107">
-              <h5>Intel Core i5-13500</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/111">
-              <h5>Intel Core i5-12600K</h5>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className={classes.actual_offer_wrapper}>
-        <a
-          href="https://crede322.github.io/TNS/#/search/?q=core%20i9"
-          className={classes.actual_offer_title}
-        >
-          Core i9
-        </a>
-        <div className={classes.content}>
-          <a
-            href="https://crede322.github.io/TNS/#/search/?q=core%20i9"
-            className={classes.offer_button}
-          >
-            <img
-              src="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/i9%2011900k"
-              alt="actual offer"
-            />
-          </a>
-          <div className={classes.offer_categories}>
-            <a href="https://crede322.github.io/TNS/#/product/101">
-              <h5>Intel Core i9-13900K</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/106">
-              <h5>Intel Core i9-12900K</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/109">
-              <h5>Intel Core i9-11900K</h5>
-            </a>
-            <a href="https://crede322.github.io/TNS/#/product/112">
-              <h5>Intel Core i9-10850K</h5>
-            </a>
-          </div>
-        </div>
-      </div>
+      <OfferExemplar
+        link="/search/?q=ryzen%205"
+        heading="Ryzen 5"
+        img="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/ryzen5"
+      >
+        <Link to="/product/6">
+          <h5>AMD Ryzen 5 1600</h5>
+        </Link>
+        <Link to="/product/9">
+          <h5>AMD Ryzen 5 3600X</h5>
+        </Link>
+        <Link to="/product/15">
+          <h5>AMD Ryzen 5 5600X</h5>
+        </Link>
+        <Link to="/product/2">
+          <h5>AMD Ryzen 5 3600</h5>
+        </Link>
+      </OfferExemplar>
+      <OfferExemplar
+        link="/search/?q=ryzen%207"
+        heading="Ryzen 7"
+        img="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/ryzen7"
+      >
+        <Link to="/product/7">
+          <h5>AMD Ryzen 7 1700X</h5>
+        </Link>
+        <Link to="/product/5">
+          <h5>AMD Ryzen 7 2700X</h5>
+        </Link>
+        <Link to="/product/1">
+          <h5>AMD Ryzen 7 3700x</h5>
+        </Link>
+        <Link to="/product/11">
+          <h5>AMD Ryzen 7 7800X3D</h5>
+        </Link>
+      </OfferExemplar>
+
+      <OfferExemplar
+        link="/search/?q=core%20i5"
+        heading="Core i5"
+        img="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/i5%2012600k?t=2024-08-18T06%3A20%3A22.262Z"
+      >
+        <Link to="/product/110">
+          <h5>Intel Core i5-12400</h5>
+        </Link>
+        <Link to="/product/104">
+          <h5>Intel Core i5-13400F</h5>
+        </Link>
+        <Link to="/product/107">
+          <h5>Intel Core i5-13500</h5>
+        </Link>
+        <Link to="/product/111">
+          <h5>Intel Core i5-12600K</h5>
+        </Link>
+      </OfferExemplar>
+
+      <OfferExemplar
+        link="/search/?q=core%20i9"
+        heading="Core i9"
+        img="https://bngqeagmdhtibgumjdyo.supabase.co/storage/v1/object/public/cpu%20images/i9%2011900k"
+      >
+        <Link to="/product/101">
+          <h5>Intel Core i9-13900K</h5>
+        </Link>
+        <Link to="/product/106">
+          <h5>Intel Core i9-12900K</h5>
+        </Link>
+        <Link to="/product/109">
+          <h5>Intel Core i9-11900K</h5>
+        </Link>
+        <Link to="/product/112">
+          <h5>Intel Core i9-10850K</h5>
+        </Link>
+      </OfferExemplar>
     </div>
   );
 };
---
+++ src/pages/mainPage/mainPage components/actual offers/OfferExemplar.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from "react";
+import { Link } from "react-router-dom";
+import classes from "./ActualOffer.module.css";
+
+interface OfferExemplarProps {
+  link: string;
+  heading: string;
+  img: string;
+}
+
+export default function OfferExemplar({
+  children,
+  link,
+  heading,
+  img,
+}: PropsWithChildren<OfferExemplarProps>) {
+  return (
+    <div>
+      <Link to={link} className={classes.actual_offer_title}>
+        {heading}
+      </Link>
+      <div className={classes.content}>
+        <Link to={link} className={classes.offer_button}>
+          <img src={img} alt="actual offer" />
+        </Link>
+        <div className={classes.offer_categories}>{children}</div>
+      </div>
+    </div>
+  );
+}
