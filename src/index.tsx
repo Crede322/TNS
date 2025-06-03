3de2033a14707def31fcd25c0f6a358aed15bfed
Crede322 <burhanov774@gmail.com>
2025-06-04 04:58:07 +0500

    fix(overall): фикс шрифтов

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-window.addEventListener("load", () => {
-  document.body.classList.add("fonts-loaded");
-});
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
